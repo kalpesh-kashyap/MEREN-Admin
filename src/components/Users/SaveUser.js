d0e28c9593a777d0902f11186730fa61eaadd5a0
Kalpesh <kashyapkalpesh555@gmail.com>
2019-10-16 23:08:22 +0530

    input handlers

--- src/components/Users/SaveUser.js
+++ src/components/Users/SaveUser.js
@@ -1,29 +1,53 @@
 import React, {Component} from 'react';
 
 class SaveUser extends Component {
+    state = {
+        firstName: null,
+        lastName: null,
+        email: null,
+        phoneNumber: null,
+        location: null,
+        aboutUser: null
+    };
+
+    inputChangeHandler = e => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    };
+
+    submitUserFormHandler = e => {
+        e.preventDefault();
+        console.log(this.state)
+
+    };
+
     render() {
         return (
             <div className="col-12 grid-margin stretch-card">
                 <div className="card">
                     <div className="card-body">
                         <h4 className="card-title">Create User</h4>
-                        <form className="forms-sample">
+                        <form className="forms-sample" onSubmit={this.submitUserFormHandler}>
                             <div className="form-group">
                                 <label htmlFor="firstName">First Name</label>
-                                <input type="text" className="form-control" id="firstName" placeholder="First Name"/>
+                                <input type="text" className="form-control" id="firstName" placeholder="First Name"
+                                       onChange={this.inputChangeHandler}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="lastName">Last Name</label>
-                                <input type="text" className="form-control" id="lastName" placeholder="Last Name"/>
+                                <input type="text" className="form-control" id="lastName" placeholder="Last Name"
+                                       onChange={this.inputChangeHandler}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="email">Email address</label>
                                 <input type="email" className="form-control" id="email"
-                                       placeholder="Email"/>
+                                       placeholder="Email" onChange={this.inputChangeHandler}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="gender">Gender</label>
-                                <select className="form-control" id="gender">
+                                <select className="form-control" id="gender" onChange={this.inputChangeHandler}>
+                                    <option value="">Select</option>
                                     <option>Male</option>
                                     <option>Female</option>
                                 </select>
@@ -41,19 +65,21 @@ class SaveUser extends Component {
                             </div>
                             <div className="form-group">
                                 <label htmlFor="phoneNumber">Phone Number</label>
-                                <input type="text" className="form-control" id="phoneNumber" placeholder="Phone Number"/>
+                                <input type="text" className="form-control" id="phoneNumber"
+                                       placeholder="Phone Number" onChange={this.inputChangeHandler}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="location">City</label>
                                 <input type="text" className="form-control" id="location"
-                                       placeholder="Location"/>
+                                       placeholder="Location" onChange={this.inputChangeHandler}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="aboutUser">About User</label>
-                                <textarea className="form-control" id="aboutUser" rows="4"/>
+                                <textarea className="form-control" id="aboutUser" rows="4"
+                                          onChange={this.inputChangeHandler}/>
                             </div>
                             <button type="submit" className="btn btn-gradient-primary mr-2">Submit</button>
-                            <button className="btn btn-light">Cancel</button>
+                            <button className="btn btn-light" type="submit">Cancel</button>
                         </form>
                     </div>
                 </div>
